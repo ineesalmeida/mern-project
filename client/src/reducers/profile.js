92663a91b94fd8e9cb49b0cb6b5d5db2cc9431e4
Ines Almeida <ines.almeida@perspectum.com>
2020-10-21 22:37:43 +0100

    finish all the actions and reducers

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
     CLEAR_PROFILE,
-    UPDATE_PROFILE
+    UPDATE_PROFILE,
+    GET_REPOS
 } from '../actions/types';
 const initialState = {
     profile: null,
@@ -21,7 +23,13 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 profile: payload,
-                loadng: false
+                loading: false
+            }
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: payload,
+                loading: false
             }
         case PROFILE_ERROR:
             return {
@@ -34,6 +42,12 @@ export default function (state = initialState, action) {
                 ...state,
                 profile: null,
                 repos: [],
+                loading: false
+            }
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: payload,
                 loading: false
             }
         default:
