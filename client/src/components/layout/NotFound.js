016617dbab8f4df77e2baace102fe9aa0d089ec2
Ines Almeida <ines.almeida@perspectum.com>
2020-10-24 18:12:00 +0100

    add heroku stuff; finalise stuff for prod

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const NotFound = () => {
+    return (
+        <Fragment>
+            <h1 className="text-primary x-large">
+                <i className="fas fa-exclamation-triangle"></i>
+                Page Not Found
+            </h1>
+            <p className="large">Sorry, this page does not exist</p>
+        </Fragment>
+    )
+}
+
+export default NotFound;
